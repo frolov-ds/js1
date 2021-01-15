8755d93947276378c2ce2a0456a3f1fb935f2872
frolov-ds <frolov-ds@yandex.ru>
2021-01-15 17:58:03 +0300

    это четвертый коммит

--- 1/2.js
+++ 1/2.js
@@ -1,6 +1,108 @@
- 
-for (let i=0; i<5; i++) {
-    setTimeout(()=>{
-        console.log(i);
-    },0);
-}
+// let num = 50;
+// if (num < 49){
+//     console.log("Неверно")
+// } else if (num > 100) {
+//     console.log("Много!")
+// } else {
+//     console.log("Верно!")
+// }
+// (num ==50) ? console.log("Верно!") : console.log("Неверно!")
+
+// switch (num) {
+//     case num < 49:
+//         console.log("Неверно");
+//         break;
+//     case num > 100:
+//         console.log("Много!");
+//         break;
+//     case num > 80:
+//         console.log("Всё еще Много!");
+//         break;
+//     case 50:
+//         console.log("Верно!");
+//         break;
+//     default:
+//         console.log("Что-то не так");
+// }
+// let num = 50;
+// // while (num <55){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 58);
+
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let num = 23;
+// function showFirstMessage(text){
+//     alert(text);
+//     num = 10;
+// }
+// showFirstMessage("hello");
+// console.log(num);
+
+// function calc(a,b) {
+//     return (a + b);
+// }
+// console.log(calc(3,4));
+// console.log(calc(2,12));
+
+// function retVar(){
+//     let num = 50;
+//     return num;
+// }
+// let out=retVar();
+// console.log(out);
+
+// let str = "дима";
+// console.log(str.length);
+// console.log(str.toUpperCase());
+
+// let twelve = "12.2";
+// console.log(Math.round(twelve));
+
+// let twelve2 = "12.2px";
+// console.log(parseInt(twelve2));
+// console.log(parseFloat(twelve2));
+
+// колбэк-функции
+// function f(dd,tt){
+//     console.log(dd);
+//     setTimeout(tt,1500);    
+// }
+// function mm(){
+//     console.log("конец паузы");
+// }
+// f("1", mm)
+
+// работа с объектами
+// let fdMonitor = {
+//     width: 1600,
+//     height: 900,
+//     name: "ViewSonic"
+// };
+// console.log(fdMonitor.name);
+
+// fdMonitor.color = {
+//     border: "black",
+//     bg: "grey"
+// };
+
+// console.log(fdMonitor);
+
+// for (let fds in fdMonitor){
+//     console.log('Свойство: ' + fds + ' имеет значение: ' + fdMonitor[fds]);
+// }
+// console.log(Object.keys(fdMonitor).length)
+
+
